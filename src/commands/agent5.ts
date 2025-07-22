9fe779335966f6245faac149954fda8d37f6843a
Asif Momin <asifdmomin@gmail.com>
2025-07-22 14:31:02 +1000

    modified package.json for faster run time

--- src/commands/agent5.ts
+++ src/commands/agent5.ts
@@ -8,7 +8,7 @@ import {agentWithLoop} from "../llm/agentWithLoop";
 export const agent5Command = new Command('agent5')
     .description('Agent5')
     .action(async (prompt: string) => {
-        await agentWithLoop("what is the current weather in melbourne?");
-        // await agentWithLoop("what activity should I do in my location?");
+        // await agentWithLoop("what is the current weather in melbourne?");
+        await agentWithLoop("what activity should I do in my location?");
         // await agentWithLoop("what are some activities idea I can do this afternoon?");
     });
